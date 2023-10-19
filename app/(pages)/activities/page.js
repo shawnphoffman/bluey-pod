e8ed706093a185e06e8627cd8019bd8f4f672b43
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-10-19 07:16:40 -0700

    Fix download links

--- app/(pages)/activities/page.js
+++ app/(pages)/activities/page.js
@@ -11,28 +11,28 @@ import colorStella from 'app/images/coloring/color_stella.png'
 const coloringPages = [
 	{
 		import: colorHenry,
-		pdf: '/color_henry.png',
-		png: '/color_henry.pdf',
+		png: '/color_henry.png',
+		pdf: '/color_henry.pdf',
 	},
 	{
 		import: colorStella,
-		pdf: '/color_stella.png',
-		png: '/color_stella.pdf',
+		png: '/color_stella.png',
+		pdf: '/color_stella.pdf',
 	},
 	{
 		import: colorAmanda,
-		pdf: '/color_amanda.png',
-		png: '/color_amanda.pdf',
+		png: '/color_amanda.png',
+		pdf: '/color_amanda.pdf',
 	},
 	{
 		import: colorEric,
-		pdf: '/color_eric.png',
-		png: '/color_eric.pdf',
+		png: '/color_eric.png',
+		pdf: '/color_eric.pdf',
 	},
 	{
 		import: colorGuitar,
-		pdf: '/color_guitar.png',
-		png: '/color_guitar.pdf',
+		png: '/color_guitar.png',
+		pdf: '/color_guitar.pdf',
 	},
 ]
 
