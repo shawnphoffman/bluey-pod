a2e3429dbfdbe9e5bc2bbdabfb0c2bd6c0e4dbf3
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-20 07:18:07 -0700

    Make goodpods configurable

--- components/AwardGoodpods.js
+++ components/AwardGoodpods.js
@@ -3,30 +3,16 @@ import Image from 'next/image'
 import styles from './AwardGoodpods.module.css'
 
 export default function AwardGoodpods() {
+	const goodpodsUrl = process.env.NEXT_PUBLIC_GOODPODS_URL
+	const goodpodsBadgeUrl = process.env.NEXT_PUBLIC_GOODPODS_BADGE_URL
+
+	if (!goodpodsUrl || !goodpodsBadgeUrl) {
+		return null
+	}
+
 	return (
-		<div className={styles.container}>
-			<a
-				href="https://goodpods.com/leaderboard/top-100-shows-by-category/leisure/animation-and-manga?indie=false&period=week#33154622"
-				target="_blank"
-			>
-				<Image
-					src="https://storage.googleapis.com/goodpods-images-bucket/leaderboard_badges/leisure_animation-and-manga_top10_week.png"
-					alt="goodpods"
-					width={250}
-					height={77}
-					className={styles.img}
-				/>
-			</a>
-			<a
-				href="https://goodpods.com/leaderboard/top-100-shows-by-category/leisure/animation-and-manga"
-				className={styles.link}
-				target="_blank"
-			>
-				Goodpods Top 100 Animation & Manga Podcasts
-			</a>
-			{/* <a href="https://goodpods.com/podcasts/dinner-with-the-heelers-a-bluey-podcast-277737" className={styles.link} target="_blank">
-				Listen now to Dinner with the Heelers - A Bluey Podcast
-			</a> */}
-		</div>
+		<a href={goodpodsUrl} target="_blank" className={styles.container}>
+			<Image src={goodpodsBadgeUrl} alt="goodpods" width={250} height={77} className={styles.img} />
+		</a>
 	)
 }
