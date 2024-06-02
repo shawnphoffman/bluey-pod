43253821ca6fbd49c77034b2c11ab7e95a051102
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-02 13:51:40 -0400

    Add word search to desktop

--- app/(pages)/activities/page.tsx
+++ app/(pages)/activities/page.tsx
@@ -6,6 +6,7 @@ import colorEric from '@/app/images/coloring/color_eric.png'
 import colorGuitar from '@/app/images/coloring/color_guitar.png'
 import colorHenry from '@/app/images/coloring/color_henry.png'
 import colorStella from '@/app/images/coloring/color_stella.png'
+import Puzzle from '@/components/core/Puzzle'
 
 const coloringPages = [
 	{
@@ -37,33 +38,40 @@ const coloringPages = [
 
 export default async function Activities() {
 	return (
-		<div className={'flex flex-col gap-4 w-full bubbled'}>
-			<div className={'text-2xl font-bold'}>Coloring Pages</div>
-			<div className={'flex flex-row justify-center overflow-hidden flex-wrap gap-2'}>
-				{coloringPages.map(coloringPage => (
-					<div className={'flex flex-col items-center'} key={coloringPage.png}>
-						<Image className="rounded-lg" alt="" src={coloringPage.import} width={200} />
-						<div className="flex flex-row gap-2">
-							<Link
-								href={coloringPage.png}
-								target="_blank"
-								className={'bg-brandText text-white p-2 rounded-[2rem] m-1 self-center w-16 leading-none'}
-								prefetch={false}
-							>
-								PNG
-							</Link>
-							<Link
-								href={coloringPage.pdf}
-								target="_blank"
-								className={'bg-brandText text-white p-2 rounded-[2rem] m-1 self-center w-16 leading-none'}
-								prefetch={false}
-							>
-								PDF
-							</Link>
+		<>
+			<div className={'flex flex-col gap-4 w-full bubbled'}>
+				<div className={'text-2xl font-bold'}>Coloring Pages</div>
+				<div className={'flex flex-row justify-center overflow-hidden flex-wrap gap-2'}>
+					{coloringPages.map(coloringPage => (
+						<div className={'flex flex-col items-center'} key={coloringPage.png}>
+							<Image className="rounded-lg" alt="" src={coloringPage.import} width={200} />
+							<div className="flex flex-row gap-2">
+								<Link
+									href={coloringPage.png}
+									target="_blank"
+									className={'bg-brandText text-white p-2 rounded-[2rem] m-1 self-center w-16 leading-none'}
+									prefetch={false}
+								>
+									PNG
+								</Link>
+								<Link
+									href={coloringPage.pdf}
+									target="_blank"
+									className={'bg-brandText text-white p-2 rounded-[2rem] m-1 self-center w-16 leading-none'}
+									prefetch={false}
+								>
+									PDF
+								</Link>
+							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
-		</div>
+
+			<div className={'flex-col gap-4 w-fit bubbled hidden md:flex'}>
+				<div className={'text-2xl font-bold'}>Word Search</div>
+				<Puzzle />
+			</div>
+		</>
 	)
 }
---
+++ components/core/Puzzle.tsx
@@ -0,0 +1,82 @@
+import { generate } from '@sbj42/word-search-generator'
+
+const height = 15
+const width = 15
+
+export default function Puzzle() {
+	const options = {
+		diagonals: true,
+		width,
+		height,
+		minLength: 3,
+		maxLength: undefined,
+		words: [
+			'Bluey',
+			'Bingo',
+			'Bandit',
+			'Chilli',
+			'Muffin',
+			'Socks',
+			'Lucky',
+			'Coco',
+			'Snickers',
+			'Rusty',
+			'Mackenzie',
+			'Indy',
+			'Honey',
+			'Jack',
+			'Chloe',
+			'Lila',
+			'Buddy',
+			'Calypso',
+			'Luckys Dad',
+			'Judo',
+			'Winton',
+			'Pretzel',
+			'JeanLuc',
+			'Alfie',
+			'Missy',
+			'Frisky',
+			'Uncle Stripe',
+			'Nana',
+			'Grandad',
+			'Uncle Rad',
+			'Trixie',
+			'Gruber',
+			'Bella',
+			'Calypso',
+			'Wendy',
+			'Doreen',
+			'Fido',
+		],
+	}
+
+	const puzzle = generate(options)
+	const rowLooper = new Array(height).fill('')
+	const colLooper = new Array(width).fill('')
+
+	return (
+		<div className="flex flex-col items-center gap-3 text-xl font-bold divide-y divide-brandBoxShadow">
+			<div className="flex flex-col items-center justify-center w-fit">
+				{rowLooper.map((_, i) => {
+					return (
+						<div className="flex flex-row items-center w-full max-w-screen-sm leading-relaxed" key={i}>
+							{colLooper.map((_, j) => {
+								return (
+									<div key={`${i}${j}`} className="flex items-center justify-center w-10">
+										{puzzle.get(j, i)}
+									</div>
+								)
+							})}
+						</div>
+					)
+				})}
+			</div>
+			<div className="flex flex-row flex-wrap justify-center max-w-3xl pt-3 gap-x-8 gap-y-2">
+				{puzzle.words.map((word, i) => (
+					<div key={word}>{word}</div>
+				))}
+			</div>
+		</div>
+	)
+}
